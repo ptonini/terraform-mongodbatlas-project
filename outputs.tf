260e514be6842c30fe2dcbb135193a41ea3d3e3f
ptonini <pedro.tonini@hotmail.com>
2023-12-18 22:42:34 -0300

    feat: removed user resource

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,3 @@
 output "this" {
   value = mongodbatlas_project.this
 }
-
-output "credentials" {
-  value = {
-    username = mongodbatlas_database_user.this.username
-    password = mongodbatlas_database_user.this.password
-  }
-}
--- variables.tf
+++ variables.tf
@@ -2,23 +2,22 @@ variable "org" {}
 
 variable "name" {}
 
-variable "name_prefix" {
-  default = "nodis"
-}
+variable "provider_name" {}
 
-variable "provider_name" {
-  default = "AZURE"
-}
-
-variable "provider_region_name" {
-  default = "US_EAST"
-}
+variable "provider_region_name" {}
 
 variable "atlas_cidr_block" {
   default = null
 }
 
 variable "peering" {
+  type = object({
+    atlas_cidr_block = string
+    azure_directory_id    = optional(string)
+    azure_subscription_id = optional(string)
+    resource_group_name   = optional(string)
+    vnet_name             = optional(string)
+  })
   default = null
 }
 
